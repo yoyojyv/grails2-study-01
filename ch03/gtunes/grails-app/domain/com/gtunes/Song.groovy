ee6c14a7fabdf192279da1e21806230373fbb5cd
yoyojyv <yoyojyv@gmail.com>
2013-04-04 11:10:59 +0900

    initialize chapter03

---
+++ ch03/gtunes/grails-app/domain/com/gtunes/Song.groovy
@@ -0,0 +1,14 @@
+package com.gtunes
+
+class Song {
+
+    String title
+    String artist
+    Album album
+
+    static constraints = {
+        title blank: false
+        artist blank: false
+    }
+
+}
