79ef439fc62db797804737dc8151dee81a9d425c
yoyojyv <yoyojyv@gmail.com>
2013-04-04 12:30:32 +0900

    change domain's relationship

--- ch03/gtunes/grails-app/domain/com/gtunes/Album.groovy
+++ ch03/gtunes/grails-app/domain/com/gtunes/Album.groovy
@@ -3,9 +3,13 @@ package com.gtunes
 class Album {
 
     String title
+    Integer year
+    String genre
 
-    static  hasMany = [songs: Song]
+    static hasMany = [songs: Song] // 1:n
 
     static constraints = {
+        title blank: false
+        year range: 1900..2100
     }
 }
--- ch03/gtunes/grails-app/domain/com/gtunes/Song.groovy
+++ ch03/gtunes/grails-app/domain/com/gtunes/Song.groovy
@@ -2,13 +2,24 @@ package com.gtunes
 
 class Song {
 
+    Date dateCreated
+    Date lastUpdated
+
     String title
-    String artist
+    String genre
+    Integer year
+    Integer trackNumber
+    Integer duration
+
     Album album
+    Artist artist
 
     static constraints = {
         title blank: false
-        artist blank: false
+        duration min: 1
+        year range:1900..2100
     }
+
+    String toString() { title }
 
 }
